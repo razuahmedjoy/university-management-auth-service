574eb4faec9f74facc8be9a1d62072f3f08c8b76
Razu Ahmed Joy <rjjoybzs420@gmail.com>
2023-06-20 14:22:55 +0600

    Improved lint setup

--- dist/app/modules/users/users.controller.js
+++ dist/app/modules/users/users.controller.js
@@ -1,35 +1,61 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict'
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value)
+          })
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const users_service_1 = __importDefault(require("./users.service"));
-const createUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected)
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next())
+    })
+  }
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+const users_service_1 = __importDefault(require('./users.service'))
+const createUser = (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const { user } = req.body;
-        const result = yield users_service_1.default.createUser(user);
-        res.status(200).json({
-            success: true,
-            message: "User created successfully",
-            data: result
-        });
+      const { user } = req.body
+      const result = yield users_service_1.default.createUser(user)
+      res.status(200).json({
+        success: true,
+        message: 'User created successfully',
+        data: result,
+      })
+    } catch (err) {
+      res.status(400).json({
+        success: false,
+        message: 'failed to create User',
+      })
     }
-    catch (err) {
-        res.status(400).json({
-            success: false,
-            message: "failed to create User"
-        });
-    }
-});
+  })
 exports.default = {
-    createUser
-};
+  createUser,
+}
