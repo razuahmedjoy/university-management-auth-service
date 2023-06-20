dfd86df640babcab2510460b3675cd3aacd3ba51
Razu Ahmed Joy <rjjoybzs420@gmail.com>
2023-06-20 14:16:26 +0600

    [Done] User service

---
+++ dist/app/modules/users/users.model.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+exports.User = (0, mongoose_1.model)('User', userSchema);
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,4 +5,5 @@ dotenv.config({ path: path.join(process.cwd(), '.env') })
 export default {
   port: process.env.PORT,
   databaseURL: process.env.DB_URL,
+  default_user_pass: process.env.DEFAULT_USER_PASS,
 }
