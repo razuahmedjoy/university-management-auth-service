257819b069362cacdf79748454342176b2e4d308
Razu Ahmed Joy <rjjoybzs420@gmail.com>
2023-06-20 12:06:53 +0600

    [Done] create User Interface and Module

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
     await mongoose.connect(config.databaseURL as string)
 
     console.log('Database connected Successfully')
+
     app.listen(config.port, () => {
       console.log(`Server is running on port http://localhost:${config.port}`)
     })
